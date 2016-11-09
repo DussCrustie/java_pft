74278063b82b3a3ef69ac07dfd14257de8d5bcf0
DussCrustie <Russianchel@mail.ru>
2016-11-09 13:22:18 +0300

    для теста точек

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,21 +4,17 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    System.out.println("Class Point");
-    Point p1 = new Point(3, 6);
-    Point p2 = new Point(8, 1);
+    hello("world");
+    hello("user");
+    hello("Alexei");
 
-      /* реализация с помощью функции*/
-    System.out.println("distance function (" + p1.x + "," + p1.y + "), (" + p2.x + "," + p2.y + ") = " + distance(p1, p2));
+    Square s=new Square(5);
+    System.out.println("Площадь квадрата со стороной" +s.l + "=" + s.area());
 
-     /* метод в классе Point*/
-    System.out.println("distance method (" + p1.x + "," + p1.y + "), (" + p2.x + "," + p2.y + ") = " + p1.distance(p2));
-  }
-  /* функция */
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-  }
-
+    Rectangle r=new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + "и" + r.b + "=" + r.area());
+    }
+ public static void hello(String somebody){
+   System.out.println("Hello," +somebody+"!" );
+ }
 }
-
-
