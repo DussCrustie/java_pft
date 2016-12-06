66eec9b8adf8b6415509b277d742aaf8b3d9ca08
DussCrustie <Russianchel@mail.ru>
2016-12-06 14:05:07 +0300

    выполнение с 9 по 12 видео

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by AKuznetsov on 06.12.2016.
+ */
+public class NavigationHelper extends HelperBase{
+
+  public NavigationHelper(FirefoxDriver wd) {
+    super(wd);
+  }
+
+  public void gotoGroupPage() {
+    click(By.linkText("groups"));
+  }
+}
